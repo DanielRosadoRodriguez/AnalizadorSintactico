e5f3b7fa321ae2bbfa6348a3e8a359feded4b1a0
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-12-13 17:09:31 -0600

    feat: add exception for when the program has already accepted every word in the text file

---
+++ src/modules/exeptions.py
@@ -0,0 +1,5 @@
+
+class CompilacionExitosaException(Exception):
+    def __init__(self, message='El programa ha terminado de compilarse'):
+        self.message = message
+        super().__init__(self.message)
