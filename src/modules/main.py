b897ea28e75c92d91f4d2839c0bbb079933832f0
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-12-13 22:39:30 -0600

    feat: add first validation in main file

--- src/modules/main.py
+++ src/modules/main.py
@@ -2,4 +2,7 @@ import validations
 import data_access
 
 file_content = data_access.read_file("./../text_files/prueba.lex")
-
+if len(validations.is_program_file(file_content)) == 0:
+    print("Compilación Exitosa")
+else:
+    print("Se han encontrado errores")
