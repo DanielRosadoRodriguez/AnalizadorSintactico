3db0c18385537cce4473426b6635951ebaccf922
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-12-14 15:20:18 -0600

    feat: add message after processing input

--- src/modules/main.py
+++ src/modules/main.py
@@ -2,5 +2,7 @@ from validations import *
 import data_access
 
 file_content = data_access.read_file("./../text_files/prueba.lex")
-print(file_content)
-print(is_program_file(file_content))
+if is_program_file(file_content) == []:
+    print("compilación exitosa")
+else:
+    print("error al compilar")
